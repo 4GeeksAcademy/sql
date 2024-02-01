361b6f2f972f74ce3aff5a4a930a74b8642a7486
SOOFIALOZA <144837565+SOOFIALOZA@users.noreply.github.com>
2024-02-01 22:20:15 +0000

    2

--- src/app.py
+++ src/app.py
@@ -3,13 +3,31 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 
-# load the .env file variables
+# Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+# Crear la URL de conexión a la base de datos
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+engine = create_engine(connection_string).execution_options(autocommit=True)
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+# Intentar conectarse
+with engine.connect() as connection:
+    # Crear las tablas si no existen
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS publishers (
+            publisher_id INT NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            PRIMARY KEY(publisher_id)
+        );
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+        -- Resto de las sentencias CREATE TABLE IF NOT EXISTS
+    """)
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+    # Insertar datos
+    engine.execute("""
+        -- Sentencias INSERT INTO ...
+    """)
+
+# Utilizar pandas para imprimir una de las tablas como un DataFrame
+result_dataFrame = pd.read_sql("SELECT * FROM publishers;", engine)
+print(result_dataFrame)
